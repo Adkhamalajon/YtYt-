ac8cdeee2420ca8605ffef84042784c72ef9257c
Adkhamalajon <adkhamalijon98@gmail.com>
2023-01-01 21:34:59 +0300

    toggle-button

--- YtYt-/main.js
+++ YtYt-/main.js
@@ -1,1 +1,11 @@
+let toggle = document.querySelector(".toggle-button")
+let navWrapper = document.querySelector(".nav-wrapper")
+toggle.addEventListener("click", ()=>{
+    navWrapper.style.display =  "flex";
+})
+let xButoon = document.querySelector(".x-button")
+undefined
+xButoon.addEventListener("click", ()=>{
+    navWrapper.style.display = "none"
+})
 
